2a2325e84c6c856cb3bcdd8d30e69ed4cf295c06
nickholo <nick.holo@outlook.com>
2024-09-13 10:18:50 -0400

    Updated about section to remove uneccesary text and change current works to a list. Changed the grid columns from 2 to 4 on screens up to xl.

--- src/components/About/AboutMe.jsx
+++ src/components/About/AboutMe.jsx
@@ -1,22 +1,16 @@
 const AboutMe = () => (
     <article className="space-y-4">
         <p>
-            I'm a passionate second-year student at Sheridan College pursuing a dual specialization in Software Development and Network Engineering. My journey in the tech world is driven by a love for creating innovative software solutions and designing robust network infrastructures that enable seamless connectivity and security.
+            I'm a passionate second-year student at Sheridan College pursuing a dual specialization in Software Development and Network Engineering. 
         </p>
         <p>
-            Throughout my studies, I've developed proficiency in a variety of programming languages, including Java, Python, and JavaScript, as well as a solid understanding of network protocols, cybersecurity principles, and systems administration. I've had the opportunity to work on diverse projects, from building responsive web applications to configuring and managing complex network systems, allowing me to apply my skills in real-world scenarios and continuously expand my technical expertise.
+            I've had the opportunity to work on diverse projects, from building responsive web applications to configuring and managing complex network systems, allowing me to apply my skills in real-world scenarios and continuously expand my technical expertise.
         </p>
         <p>
-            I'm particularly interested in cloud computing, web development, artificial intelligence, and I'm always eager to learn and explore new technologies that push the boundaries of what's possible. My goal is to combine my software development skills with my network engineering knowledge to create scalable, secure, and efficient solutions that make a meaningful impact.
+            I'm particularly interested in cloud computing, web development, artificial intelligence, and I'm always eager to learn and explore new technologies that push the boundaries of what's possible.
         </p>
         <p>
             In addition to my technical abilities, I pride myself on being a collaborative team player with strong problem-solving skills and a keen attention to detail. I thrive in dynamic environments where creativity and critical thinking are encouraged, and I enjoy taking on challenges that allow me to grow both personally and professionally.
-        </p>
-        <p>
-            When I'm not immersed in coding or network configurations, you can find me gaming, reading, or working on my car or motorcycle, which help me maintain a balanced and inspired mindset.
-        </p>
-        <p>
-            Feel free to explore my portfolio to see some of the projects I've worked on, and don't hesitate to reach out if you'd like to connect or collaborate!
         </p>
     </article>
 )
--- src/components/Skills/SkillsGrid.jsx
+++ src/components/Skills/SkillsGrid.jsx
@@ -2,7 +2,7 @@ import SkillCard from './SkillCard';
 import {MySkills} from '../utils/constants'
 
 const SkillsGrid = () => (
-    <div className='grid grid-cols-2 gap-4 w-full md:grid-cols-4 xl:grid-cols-8'>
+    <div className='grid grid-cols-4 gap-4 w-full xl:grid-cols-8'>
         {MySkills.map((skill, index) => (
             <SkillCard key={index} skill={skill} />
         ))}
