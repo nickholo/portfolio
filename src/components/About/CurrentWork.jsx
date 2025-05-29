8b5c960f53d96debb0fe6f7a79fa7be6568b399a
nickholo <hologron@sheridanc.on.ca>
2025-05-28 23:42:01 -0400

    Update CurrentWork.jsx

--- src/components/About/CurrentWork.jsx
+++ src/components/About/CurrentWork.jsx
@@ -2,18 +2,18 @@ const CurrentWork = () => (
     <article className="mt-10 space-y-4">
         <h3 className='text-3xl font-semibold'>What am I currently working on?</h3>
         <ul className='list-disc list ml-4 mt-4'>
-            <li>A web application to track the stock and crypto markets giving predictions based on regularly updated data.
+            <li>An interactive periodic table web application.
                 <ul className='list-disc list-inside'>
                     <li>React</li>
-                    <li>Spring Boot</li>
-                    <li>Python ML</li>
+                    <li>TypeScript</li>
+                    <li>TailwindCSS</li>
                 </ul>
             </li>
-            <li className='mt-4'>A home server to host various web applications and store personal data
+            <li className='mt-4'>A platforming game about my silly little dog
                 <ul className='list-disc list-inside'>
-                    <li>Linux</li>
-                    <li>Network administration</li>
-                    <li>PC building</li>
+                    <li>Java</li>
+                    <li>LibGDX</li>
+                    <li>Asset Management & Design</li>
                 </ul>
             </li>
 
